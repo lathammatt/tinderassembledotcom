84105168c5dc8f7c14902b101dc6861dffc9c583
Jamey McAuliffe <jpmcauliffe15@hotmail.com>
2016-09-28 14:02:00 -0500

    profile.js created

--- routes/profile.js
+++ routes/profile.js
@@ -2,15 +2,19 @@
 
 const { Router } = require('express')
 const bcrypt = require('bcrypt')
-
+const User = require('../models/user')
+const { json } = require('body-parser')
 const router = Router()
 
 router.get('/profile', (req, res) => {
 	res.send('profile')
 })
 
-router.put('/profile/:id', (req, res) => {
-	
+router.post('/profile/:id', (req, res, err) => {
+	User
+		.create(req.body)
+		.then(user => res.send(user))
+		.catch(err)
 })
 
 module.exports = router
